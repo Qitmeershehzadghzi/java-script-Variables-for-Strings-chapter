f55cfaa327b6b4f8738f76d84803273d4744e115
Qitmeershehzad <qitmeershehzad@gmail.com>
2024-03-28 16:26:37 +0500

    Java script 2nd chapter

---
+++ app.js
@@ -0,0 +1,10 @@
+let userName ="Qitmeer"
+console.log(userName);
+let userFname ="shehzad"
+console.log(userFname);
+let rollNumber ="181736"
+console.log(rollNumber);
+let age ="15";
+console.log(age);
+let nationality ="pakistan"
+console.log(nationality);
